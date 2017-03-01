cb10310e9f6dadfd766beb7dc64772d3b89810cc
Alvin Law <alvinlaw@gmail.com>
2017-02-28 20:31:37 -0800

    add comments to drill 5

--- w1d2/05-func-return/app.js
+++ w1d2/05-func-return/app.js
@@ -1,11 +1,13 @@
+//return a function that console logs a greeting and name.
 function createGreeter(greeting) {
   return function(name) {
     console.log(`${greeting} ${name}`);
   };
 }
-
+//create function variables for different types of greeting.
 var englishGreeting = createGreeter('Hello');
 var frenchGreeting = createGreeter('Bonjour');
 
+//run english and french greeting.
 englishGreeting('Anna');
 frenchGreeting('Sofia');
